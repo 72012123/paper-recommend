929efe4de5bf146c155c7eadd573c531921e4985
72012123 <cv789789z@gmail.com>
2019-11-11 20:00:35 +0800

    Add files via upload

--- paper_recommend.py
+++ paper_recommend.py
@@ -6,9 +6,11 @@ cursor = conn.cursor()
 
 wb = xw.Book("author.xlsx")
 wc = xw.Book("keyword.xlsx")
+wd = xw.Book()
 
 read_author = wb.sheets[0]
 read_keyword = wc.sheets[0]
+writeKeyword = wd.sheets['工作表1']
 
 author = np.zeros(156384)
 author = author.astype(int)
@@ -30,7 +32,7 @@ for i in range(156384):
 for i in range(20343):
     keyword[i] = b[i]
 
-for i in range(2):#依照a_id找到每篇論文的年份並分類
+for i in range(156384):#依照a_id找到每篇論文的年份並分類
     temp = author[i]
     sql = "SELECT * FROM `paper_author` WHERE `a_id` = %s"%(str(temp))
     cursor.execute(sql)
@@ -53,7 +55,7 @@ for i in range(2):#依照a_id找到每篇論文的年份並分類
             yearClassification[2017 - result[3]][record] = paper[j]
             #print(author[i], yearClassification[2017 - result[3]][record], result[3], record)
 
-    for j in range(8):
+    for j in range(8):#每個作者所參與的論文轉換成keyword
         for r in range(300):
             if yearClassification[j][r] != 0:
                 tempPaper = yearClassification[j][r]
@@ -63,10 +65,30 @@ for i in range(2):#依照a_id找到每篇論文的年份並分類
                 keywordResult = cursor.fetchall()
                 keywordPosition = 0
                 for keywordRow in keywordResult:
-                    tempKeyword = keywordRow[1]
-                    print(author[i], yearClassification[j][r], tempKeyword)#有關鍵字不在table裡面
-
+                    tempKeyword = keywordRow[1]#拿到該篇的keyword
+                    while (keywordPosition < 20342) and (keyword[keywordPosition] < int(tempKeyword)):
+                        keywordPosition = keywordPosition + 1
+                    if keyword[keywordPosition] == int(tempKeyword):
+                        sortKeyword[j][keywordPosition] = sortKeyword[j][keywordPosition] + 1
+                        #print(2017-j, author[i], yearClassification[j][r], tempKeyword, sortKeyword[j][keywordPosition])
+    for j in range(8):#把每個年份最常出現的keyword排序
+        for k in range(5):
+            maxPosition = 0
+            maxKeywordNumber = 0
+            for r in range(20343):
+                if sortKeyword[j][r] > maxKeywordNumber:
+                    maxPosition = r
+                    maxKeywordNumber = sortKeyword[j][r]
+            if maxKeywordNumber == 0:
+                print(2017 - j, author[i], 0, 0)
+                writeKeyword.cells(i + 1, (k + 1) + j * 5).value = 0
+            else:
+                print(2017 - j, author[i], keyword[maxPosition], sortKeyword[j][maxPosition])
+                writeKeyword.cells(i + 1, (k + 1) + j * 5).value = keyword[maxPosition]
+            sortKeyword[j][maxPosition] = 0
     for j in range(8):
         for r in range(300):
             yearClassification[j][r] = 0
+        for k in range(20343):
+            sortKeyword[j][k] = 0
 conn.close()
